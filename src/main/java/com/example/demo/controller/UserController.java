174bc13fb384fbf5aeba7c22602534ce2667bed3
Justin Kim <justinkim2015@gmail.com>
2023-03-15 22:05:33 +0900

    Update app to get data from db

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+
+    UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    List<User> all() {
+        return userRepository.findAll();
+    }
+}
